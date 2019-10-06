533012604c0facb5c1031dc8196fae0205694555
Yaobin Ling <yling1105@outlook.com>
2019-10-05 20:15:09 -0500

    Create ui.R

---
+++ shiny app/ui.R
@@ -0,0 +1,61 @@
+library(shiny)
+library(shinythemes)
+
+# Define UI for miles per gallon application
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Body fat Calculator"),
+  
+  # and to specify whether outliers should be included
+  sidebarPanel(
+    numericInput(
+      "num1",
+      h3("Put in your Abdomen 2 circumference:"),
+      value = NULL
+    ), 
+    
+    selectInput(
+      "unit1", 
+      "Units:",
+      list("cm" = "cm",
+           "inch" = "inch")
+    ),
+    
+    helpText("Note: Please enter a correct measurement, 
+             our App can only work efficiently for Abdomen 
+             ranges from 60cm to 130cm(23.62 inches ~ 51.181 inches)"),
+    
+    numericInput(
+      "num2",
+      h3("Put in your Wrist circumference:"),
+      value = NULL
+    ),
+    
+    selectInput(
+      "unit2", 
+      "Units:",
+      list("cm" = "cm", 
+           "inch" = "inch")
+    ),
+    helpText("Note: Please enter a correct measurement, 
+             our App can only work efficiently for Abdomen 
+             ranges from 15cm to 22cm(5.9055 inches ~ 8.6614 inches)"),
+    
+    radioButtons("radio", h3("Gender"),
+                 choices = list("Male" = "Male", "Female" = "Female"),
+                 selected = "Male"),
+  
+    actionButton(inputId = "go", label = "Submit")
+  ),
+  
+  # Show the caption and plot of the requested variable against mpg
+  mainPanel(
+    h2("Result:"),
+    textOutput("bodyfat"),
+    textOutput("tips"),
+    img(src = 'badger.png', height = 500, width = 350),
+    br()
+
+  )
+))
